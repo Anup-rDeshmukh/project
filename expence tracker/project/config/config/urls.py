3e9a4fac85e668b2c3680f27c12f21ba19932118
Anup Deshmukh <anupdeshmukh503@gmail.com>
2026-03-17 21:44:13 +0530

    login page added

--- expence tracker/project/config/config/urls.py
+++ expence tracker/project/config/config/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from tracker import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.dashboard),
+    path('', include('tracker.urls')),
 ]
+
+
--- expence tracker/project/config/tracker/views.py
+++ expence tracker/project/config/tracker/views.py
@@ -9,3 +9,10 @@ def dashboard(request):
             print(uploaded_file.name)  
 
     return render(request, "index.html")
+
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def signin(request):
+    return render(request,'signin.html')
